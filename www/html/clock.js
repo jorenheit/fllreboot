b46e74b8856424b13520b5183cbc6366a4c51250
Joren Heit <jorenheit@gmail.com>
2020-02-10 11:44:00 +0100

    Previous commit didn't have proper files staged.

---
+++ www/html/clock.js
@@ -0,0 +1,8 @@
+/* 
+   Used by clock.php to display the clock.
+*/
+
+function clock()
+{
+    document.getElementById("clock").innerHTML = "00:00:00"; 
+}
---
+++ www/html/style.php
@@ -0,0 +1,20 @@
+<?php
+/*
+  Functions that can be called from clock.php to determine the 
+  style of the clock, depending on the ID of the device that
+  is running the browser.
+ */
+
+
+function getBackgroundColor($id) {
+    return "lightgreen";
+}
+
+function getBackgroundImage($id) {
+    return "";
+}
+
+function getFontColor($id) {
+    return "blue";
+}
+?>
